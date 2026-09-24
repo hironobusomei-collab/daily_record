31290aaf5854e92546c07177f7f07c6c3253a4d7
hironobusomei-collab <hironobusomei@gmail.com>
2026-09-24 16:33:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="daily-record-v20-clean";
+const CACHE="daily-record-v21-fixed";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./apple-touch-icon.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
